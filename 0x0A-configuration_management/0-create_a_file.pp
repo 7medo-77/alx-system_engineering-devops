93f7b495f157b168ed166f26c2d26c483fc59617
7medo_77 <ahmedabdelhamid1997@gmail.com>
2024-04-19 18:19:32 -0700

    0-create_a_file.pp_18:19

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Puppet manifest which creates a file 'school' and ensures it's installed
-file { 'tmp/' : 
-	path => 'tmp/school',
-	ensure => present,
-	owner => www-data,
-	group => www-data,
-	permission => '0744',
-	contains => 'I love Puppet',
+file { 'tmp/' :
+  ensure     => present,
+  path       => 'tmp/school',
+  owner      => www-data,
+  group      => www-data,
+  permission => '0744',
+  contains   => 'I love Puppet',
 }
