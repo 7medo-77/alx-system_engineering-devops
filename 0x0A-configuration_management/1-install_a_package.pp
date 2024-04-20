a1ec3d0f91abeb528262b7a0e68962d914deb124
7medo_77 <ahmedabdelhamid1997@gmail.com>
2024-04-19 19:04:23 -0700

    1-install_a_package.pp_19:04

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #!/usr/bin/pup
-# Puppet manifest which installs flask package and ensures it's version 2.1.0
+# Install an especific version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip3',
+  provider => 'pip3'
 }
