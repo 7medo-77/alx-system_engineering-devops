1567a6ded1c8e7d00864f9a403b90a3a6486f143
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-24 10:41:24 +0200

    100-puppet_ssh_config.pp_10:41

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,9 @@
 # Puppet manifest which creates an ssh config file 'ssh_config'
-exec { '~/.ssh/ssh_config' :
-  command  => 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> ~/.ssh/ssh_config',
-  mode     => '0600',
-  onlyif   => 'stat ~/.ssh/ssh_config',
+augeas { 'ssh_config' :
+  context  => '~/.ssh/ssh_config',
+  changes  => [
+    "touch ~/.ssh/ssh_config",
+    "set PasswordAuthentication no",
+    ": set IdentityFile ~/.ssh/school",
+  ]
 }
