f302c3de2eeb062b4faf65a85a5ff1f25630eb3f
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-24 10:22:13 +0200

    100-puppet_ssh_config.pp_10:22

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # Puppet manifest which creates an ssh config file 'ssh_config'
-exec { '~/.ssh/ssh_config' :
-  command => 'mkdir -p ~/.ssh; touch ~/.ssh/authorized_keys; echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school"',
+file { '~/.ssh/ssh_config' :
+  ensure  => present,
+  path    => '/etc/.ssh/ssh_config',
   mode    => '0600',
-  unless  => 'stat ~/.ssh/authorized_keys'
   content => 'PasswordAuthentication no\nIdentityFile ~/.ssh/school',
 }
