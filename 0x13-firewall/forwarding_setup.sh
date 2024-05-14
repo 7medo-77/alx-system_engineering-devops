478707d64e221e7fa4c34f4e052dfc68d7b04c9f
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-05-14 05:26:19 +0300

    100-port_forwarding_05:26

--- 0x13-firewall/forwarding_setup.sh
+++ 0x13-firewall/forwarding_setup.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-sed "s/"
+NAT_config="# NAT Table configuration to set up port forwarding\n\*nat\n\:PREROUTING ACCEPT \[0:0\]\n-A PREROUTING -p tcp --dport 8080 -j REDIRECT --to-port 80\nCOMMIT\n"
+
+sed -i "s/\#net\.ipv4\.ip_forward=1/net\.ipv4\.ip_forward=1/" /etc/sysctl.conf
+sed -i "/^# End/a $NAT_config" /etc/ufw/before.rules
+sudo ufw allow 8080/tcp
+sudo systemctl restart ufw
