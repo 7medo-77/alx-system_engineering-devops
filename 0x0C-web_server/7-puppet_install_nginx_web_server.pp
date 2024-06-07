0213a89d411a28f36a882e680b9e5118c718f52e
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-06-07 21:24:08 +0300

    7-puppet_install_nginx_web_server.pp_21:24

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,31 @@
+exec {'configuration':
+  command => "sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/youtube.com\/;\\n\\t}/" /etc/nginx/sites-available/default",
+  provider => shell,
+}
+
+exec {'update':
+  command => 'sudo apt-get -y update',
+  provider => shell
+}
+
+package {'nginx':
+  ensure   => installed,
+  provider => apt,
+}
+
+file {'index.html':
+  ensure  => present,
+  content => 'Hello World!',
+  path    => '/var/www/html/',
+}
+
+file {'404.html':
+  ensure  => present,
+  content => 'Ceci n\'est pas une page',
+  path    => '/var/www/html/',
+}
+
+exec {'restart':
+  command => 'sudo service nginx restart',
+  provider => shell
+}
